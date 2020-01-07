109f65f9f49fddfd7878e26d7bea1658a4038d1d
Kamome Shido <shidokamo@users.noreply.github.com>
2020-01-07 04:34:51 +0000

    favicon のパッケージを削除

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,7 +5,6 @@ const webpack = require("webpack");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = merge(common, {
@@ -42,12 +41,6 @@ module.exports = merge(common, {
       safe: 'dotenv.example', // load '.env.example' to verify the '.env' variables are all set. Can also be a string to a different file.
       systemvars: true, // load all the predefined 'process.env' variables which will trump anything local per dotenv specs.
       defaults: false // load '.env.defaults' as the default values if empty.
-    }),
-    new FaviconsWebpackPlugin({
-      logo: './public/logo.png',
-      publicPath: "/",
-      prefix: "assets/",
-      inject: 'force',
     }),
   ]
 });
