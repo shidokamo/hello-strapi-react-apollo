bb7f42b5221d031aa1aef8f08e8a2b6e307009e3
Kamome Shido <shidokamo@users.noreply.github.com>
2020-01-08 02:14:39 +0000

    ApolloProvider の名前空間を修正

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ApolloProvider } from 'react-apollo';
+import { ApolloProvider } from '@apollo/client';
 
 import './index.css';
 import App from './containers/App';
