9964e797271155fcb14282394fef597c4381fb63
Kamome Shido <shidokamo@users.noreply.github.com>
2020-01-08 17:34:31 +0000

    パッケージのパスを v3.0 に合わせて変更

--- src/apollo-client.js
+++ src/apollo-client.js
@@ -1,21 +1,19 @@
-import { ApolloLink } from 'apollo-client-preset';
-import ApolloClient from 'apollo-client';
-import { HttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloClient, HttpLink, ApolloLink } from "@apollo/client";
+import { InMemoryCache } from "apollo-cache-inmemory";
 
-import auth from './utils/auth';
+import auth from "./utils/auth";
 
 const httpLink = new HttpLink({ uri: `http://localhost:1337/graphql` });
 
 // Middleware to set the headers
 const middlewareAuthLink = new ApolloLink((operation, forward) => {
   const token = auth.getToken();
-  const authorizationHeader = token ? `Bearer ${token}` : null
+  const authorizationHeader = token ? `Bearer ${token}` : null;
   operation.setContext({
     headers: {
-      authorization: authorizationHeader,
-    },
-  })
+      authorization: authorizationHeader
+    }
+  });
   return forward(operation);
 });
 
@@ -25,19 +23,19 @@ const httpLinkWithAuthToken = middlewareAuthLink.concat(httpLink);
 // to increase performances
 const defaultOptions = {
   watchQuery: {
-    fetchPolicy: 'network-only',
-    errorPolicy: 'ignore',
+    fetchPolicy: "network-only",
+    errorPolicy: "ignore"
   },
   query: {
-    fetchPolicy: 'network-only',
-    errorPolicy: 'all',
-  },
+    fetchPolicy: "network-only",
+    errorPolicy: "all"
+  }
 };
 
 const client = new ApolloClient({
   link: httpLinkWithAuthToken,
   cache: new InMemoryCache(),
-  defaultOptions: defaultOptions,
-})
+  defaultOptions: defaultOptions
+});
 
 export default client;
