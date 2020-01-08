e0abf4f68bab04fac247110f25fcc6d0dd65672d
Kamome Shido <shidokamo@users.noreply.github.com>
2020-01-08 18:09:59 +0000

    パッケージのパスを変更

--- src/containers/ProductDetailsPage/index.js
+++ src/containers/ProductDetailsPage/index.js
@@ -3,13 +3,17 @@
  * ProductDetailsPage
  */
 
-import React from 'react';
-import { Query } from 'react-apollo';
-import { Link } from 'react-router-dom';
-import { GET_PRODUCT } from '../../queries';
+import React from "react";
+import { Query } from "@apollo/react-components";
+import { Link } from "react-router-dom";
+import { GET_PRODUCT } from "../../queries";
 
-const ProductDetailsPage = (props) => {
-  const { match: { params: { id } } } = props;
+const ProductDetailsPage = props => {
+  const {
+    match: {
+      params: { id }
+    }
+  } = props;
 
   return (
     <Query query={GET_PRODUCT} variables={{ id }}>
@@ -26,38 +30,36 @@ const ProductDetailsPage = (props) => {
           <div>
             {Object.keys(data).map((value, key) => (
               <div key={key}>
-                <h1>
-                  {data[value].name}
-                </h1>
+                <h1>{data[value].name}</h1>
                 <div style={styles.linkWrapper}>
                   <Link to="/products">Go back to products page</Link>
                 </div>
-                {
-                  Object.keys(data[value]).map((child, key) => {
-                    // Display all the informations
-                    return (
-                      <div key={key}>
-                        <ul>
-                          <li>{child}: {data[value][child]}</li>
-                        </ul>
-                      </div>
-                    );
-                  })
-                }
+                {Object.keys(data[value]).map((child, key) => {
+                  // Display all the informations
+                  return (
+                    <div key={key}>
+                      <ul>
+                        <li>
+                          {child}: {data[value][child]}
+                        </li>
+                      </ul>
+                    </div>
+                  );
+                })}
               </div>
             ))}
           </div>
-        )
+        );
       }}
     </Query>
   );
-}
+};
 
 const styles = {
   linkWrapper: {
-    marginTop: '10px',
-    marginBottom: '10px',
-  },
+    marginTop: "10px",
+    marginBottom: "10px"
+  }
 };
 
 export default ProductDetailsPage;
--- src/containers/ProductsPage/index.js
+++ src/containers/ProductsPage/index.js
@@ -4,28 +4,28 @@
  *
  */
 
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Link }  from 'react-router-dom';
-import { Query } from 'react-apollo';
-import { GET_PRODUCTS } from '../../queries';
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import { Query } from "@apollo/react-components";
+import { GET_PRODUCTS } from "../../queries";
 
-import Table from '../../components/Table';
+import Table from "../../components/Table";
 // Utils
 
-import './styles.scss';
+import "./styles.scss";
 
 class ProductsPage extends React.Component {
   state = { products: [] };
 
-  onClick = (id) => {
+  onClick = id => {
     this.props.history.push(`/product/${id}`);
-  }
+  };
 
   render() {
     return (
       <Query query={GET_PRODUCTS}>
-        {({ loading, error, data}) => {
+        {({ loading, error, data }) => {
           if (loading) {
             return null;
           }
@@ -48,7 +48,7 @@ class ProductsPage extends React.Component {
                 <div className="row">
                   <Table
                     data={data.products}
-                    headers={['_id', 'name', 'description', '']}
+                    headers={["_id", "name", "description", ""]}
                     onClick={this.onClick}
                   />
                 </div>
@@ -64,7 +64,7 @@ class ProductsPage extends React.Component {
 ProductsPage.defaultProps = {};
 
 ProductsPage.propTypes = {
-  history: PropTypes.object.isRequired,
+  history: PropTypes.object.isRequired
 };
 
 export default ProductsPage;
