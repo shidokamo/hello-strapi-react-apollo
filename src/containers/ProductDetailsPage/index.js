291376282a4f3b397f8ce348755c5516e13303bc
Kamome Shido <shidokamo@users.noreply.github.com>
2020-01-11 05:28:11 +0000

    Apollo v3.0 の react-hook を使ったコンポーネントに書き換え

--- src/containers/ProductDetailsPage/index.js
+++ src/containers/ProductDetailsPage/index.js
@@ -4,7 +4,7 @@
  */
 
 import React from 'react';
-import { Query } from '@apollo/react-components';
+import { useQuery } from '@apollo/client';
 import { Link } from 'react-router-dom';
 import { GET_PRODUCT } from '../../queries';
 import unified from 'unified';
@@ -20,52 +20,44 @@ const ProductDetailsPage = props => {
     },
   } = props;
 
+  const { loading, error, data } = useQuery(GET_PRODUCT, {
+    variables: { id: id },
+  });
+  if (loading) return <p>loading...</p>;
+  if (error) return <p>GraphQL error</p>;
+
+  const { contents } = unified()
+    .use(markdown) // パーサー(文字列をremarkの構文木に変換)
+    .use(slug) // トランスフォーマー(章にidをつける)
+    .use(remark2rehype) // トランスフォーマー(マークダウンからHTMLに変換)
+    .use(html) // コンパイラー(HTML構文木を文字列に変換)
+    .processSync(data.product.description);
+
   return (
-    <Query query={GET_PRODUCT} variables={{ id }}>
-      {({ loading, error, data }) => {
-        if (loading) {
-          return null;
-        }
-
-        if (error) {
-          return `Error: ${error}`;
-        }
-
-        const { contents } = unified()
-          .use(markdown) // パーサー(文字列をremarkの構文木に変換)
-          .use(slug) // トランスフォーマー(章にidをつける)
-          .use(remark2rehype) // トランスフォーマー(マークダウンからHTMLに変換)
-          .use(html) // コンパイラー(HTML構文木を文字列に変換)
-          .processSync(data.product.description);
-
-        return (
-          <div>
-            {Object.keys(data).map((value, key) => (
-              <div key={key}>
-                <div style={styles.linkWrapper}>
-                  <Link to="/products">Go back to products page</Link>
-                </div>
-                <div style={styles.border}></div>
-                <div dangerouslySetInnerHTML={{ __html: contents }} />
-                <div style={styles.border}></div>
-                {Object.keys(data[value]).map((child, key) => {
-                  // Display all the informations
-                  return (
-                    <div key={key}>
-                      <ul>
-                        <li>
-                          {child}: {data[value][child]}
-                        </li>
-                      </ul>
-                    </div>
-                  );
-                })}
-              </div>
-            ))}
+    <div>
+      {Object.keys(data).map((value, key) => (
+        <div key={key}>
+          <div style={styles.linkWrapper}>
+            <Link to="/products">Go back to products page</Link>
           </div>
-        );
-      }}
-    </Query>
+          <div style={styles.border}></div>
+          <div dangerouslySetInnerHTML={{ __html: contents }} />
+          <div style={styles.border}></div>
+          {Object.keys(data[value]).map((child, key) => {
+            // Display all the informations
+            return (
+              <div key={key}>
+                <ul>
+                  <li>
+                    {child}: {data[value][child]}
+                  </li>
+                </ul>
+              </div>
+            );
+          })}
+        </div>
+      ))}
+    </div>
   );
 };
 
@@ -78,7 +70,7 @@ const styles = {
     borderTop: 'solid',
     borderWidth: '5px',
     borderColor: 'red',
-  }
+  },
 };
 
 export default ProductDetailsPage;
