5434a7822cfedc9568646fa18c968ffde4f927b2
Kamome Shido <shidokamo@users.noreply.github.com>
2020-01-11 05:18:16 +0000

    個々の product の詳細ページでもマークダウンをHTMLに変換

--- src/containers/ProductDetailsPage/index.js
+++ src/containers/ProductDetailsPage/index.js
@@ -7,6 +7,11 @@ import React from 'react';
 import { Query } from '@apollo/react-components';
 import { Link } from 'react-router-dom';
 import { GET_PRODUCT } from '../../queries';
+import unified from 'unified';
+import markdown from 'remark-parse';
+import slug from 'remark-slug';
+import remark2rehype from 'remark-rehype';
+import html from 'rehype-stringify';
 
 const ProductDetailsPage = props => {
   const {
@@ -26,14 +31,23 @@ const ProductDetailsPage = props => {
           return `Error: ${error}`;
         }
 
+        const { contents } = unified()
+          .use(markdown) // パーサー(文字列をremarkの構文木に変換)
+          .use(slug) // トランスフォーマー(章にidをつける)
+          .use(remark2rehype) // トランスフォーマー(マークダウンからHTMLに変換)
+          .use(html) // コンパイラー(HTML構文木を文字列に変換)
+          .processSync(data.product.description);
+
         return (
           <div>
             {Object.keys(data).map((value, key) => (
               <div key={key}>
-                <h1>{data[value].name}</h1>
                 <div style={styles.linkWrapper}>
                   <Link to="/products">Go back to products page</Link>
                 </div>
+                <div style={styles.border}></div>
+                <div dangerouslySetInnerHTML={{ __html: contents }} />
+                <div style={styles.border}></div>
                 {Object.keys(data[value]).map((child, key) => {
                   // Display all the informations
                   return (
@@ -60,6 +74,11 @@ const styles = {
     marginTop: '10px',
     marginBottom: '10px',
   },
+  border: {
+    borderTop: 'solid',
+    borderWidth: '5px',
+    borderColor: 'red',
+  }
 };
 
 export default ProductDetailsPage;
