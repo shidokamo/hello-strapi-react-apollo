cd4a6d3b8f160fc58087de2751cc53b285a19fa5
Kamome Shido <shidokamo@users.noreply.github.com>
2020-01-12 06:15:15 +0000

    EditPage を全面的に手直し中（Hook を使う。RESTではなく GraphQL Mutation を使う）

--- src/containers/EditPage/index.js
+++ src/containers/EditPage/index.js
@@ -4,21 +4,15 @@
  *
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 // import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { findIndex, get } from 'lodash';
 // Components
 import Button from '../../components/Button';
 import Input from '../../components/InputsIndex';
 
-import client from '../../apollo-client';
-import { GET_PRODUCT } from '../../queries';
-// Utils
-import request from '../../utils/request';
-
-// Layout
-import layout from './layout.json';
+import { useQuery, useMutation } from '@apollo/client';
+import { GET_PRODUCT, CREATE_PRODUCT, UPDATE_PRODUCT } from '../../queries';
 
 import './styles.scss';
 
@@ -27,142 +21,71 @@ const FILE_RELATIONS = {
   product: [{ name: 'pictures', multiple: true }],
 };
 
-/* eslint-disable  array-callback-return */
-class EditPage extends React.Component {
-  state = {
-    didCheckErrors: false,
-    errors: [],
-    inititalData: {},
-    modifiedData: {},
-  };
+const EditPage = props => {
+  const [description, setDescription] = useState('');
+  const [name, setName] = useState('');
 
-  async componentDidMount() {
-    if (this.props.match.params.id !== 'create') {
-      const {
-        match: { params },
-      } = this.props;
+  const title =
+    props.id === 'create'
+      ? `Create a new ${props.contentType}` // You can get contentType from Router
+      : `Edit ${props.id}`;
 
-      // FETCH data using graphql
-      const { data } = await client.query({
-        query: GET_PRODUCT,
-        variables: { id: params.id },
-      });
+  if (props.id !== 'create') {
+    const { loading, error, data } = useQuery(GET_PRODUCT, {
+      variables: { id: props.id },
+    });
+    if (loading) return <p>loading...</p>;
+    if (error) return <p>GraphQL error</p>;
+    setName(data.product.name);
+    setDescription(data.product.description);
 
-      this.setState({
-        inititalData: data[params.contentType],
-        modifiedData: data[params.contentType],
-      });
-    }
+    const [updateProduct] = useMutation(UPDATE_PRODUCT);
+  } else {
+    const [createProduct] = useMutation(CREATE_PRODUCT);
   }
 
-  // Reset form to its inital value
-  cancel = () => this.setState({ modifiedData: this.state.inititalData });
-
   handleChange = e => {
-    const name = e.target.name;
-
-    this.setState({
-      modifiedData: { ...this.state.modifiedData, [name]: e.target.value },
-    });
+    setName(e.target.name);
+    setDescription(e.target.value);
+    console.log(name, description);
   };
 
   handleSubmit = e => {
     e.preventDefault();
-    // TODO handle errors
-    const {
-      match: { params },
-    } = this.props;
-    const { modifiedData } = this.state;
-    const body = Object.keys(modifiedData).reduce((acc, current) => {
-      if (
-        findIndex(FILE_RELATIONS[params.contentType], ['name', current]) === -1
-      ) {
-        acc[current] = modifiedData[current];
-      } else {
-        const alreadyUploadedFiles = modifiedData[current].filter(file => {
-          if (file instanceof File === false) {
-            return file;
-          }
-        });
-        acc[current] = alreadyUploadedFiles;
-      }
-      return acc;
-    }, {});
-
-    const method = params.id === 'create' ? 'POST' : 'PUT';
-    const requestURL =
-      params.id === 'create'
-        ? `${process.env.STRAPI_URL}/${params.contentType}`
-        : `${process.env.STRAPI_URL}/${params.contentType}/${params.id}`;
-    return request(requestURL, { method, body: body })
-      .catch(err => {
-        console.log('err', err.response);
-        //  TODO: Handle errors
-      })
-      .finally(() => {
-        // TODO: make sure the redirection happens when all the files have been updated
-        this.props.history.push(`/${params.contentType}s`);
+    if (props.id !== 'create') {
+      updateProduct({
+        variables: { id: props.id, name: name, description: description },
       });
+    } else {
+      createProduct({ variables: { name: name, description: description } });
+      input.value = '';
+    }
+    // TODO:
+    // catch -> finally ->
+    //         this.props.history.push(`/${params.contentType}s`);
   };
 
-  render() {
-    const {
-      match: { params },
-    } = this.props;
-    const title =
-      params.id === 'create'
-        ? `Create a new ${params.contentType}`
-        : `Edit ${params.id}`;
-    const display = layout[params.contentType];
-
-    return (
-      <div className="editPageWrapper">
-        <div className="container-fluid">
-          <h1>{title}</h1>
-          <Link to={`/${params.contentType}s`}>Back</Link>
-          <form className="formWrapper" onSubmit={this.handleSubmit}>
-            <div className="row">
-              {display.map(input => (
-                <Input
-                  didCheckErrors={this.state.didCheckErrors}
-                  errors={get(
-                    this.state.errors,
-                    [
-                      findIndex(this.state.errors, ['name', input.name]),
-                      'errors',
-                    ],
-                    [],
-                  )}
-                  key={input.name}
-                  label={input.name}
-                  name={input.name}
-                  onChange={this.handleChange}
-                  type={input.type}
-                  value={get(
-                    this.state.modifiedData,
-                    input.name,
-                    input.type === 'file' ? [] : '',
-                  )}
-                  multiple
-                />
-              ))}
+  return (
+    <div className="editPageWrapper">
+      <div className="container-fluid">
+        <h1>{title}</h1>
+        <Link to={`/${params.contentType}s`}>Back</Link>
+        <form className="formWrapper" onSubmit={handleSubmit}>
+          <div className="row">
+            <form onSubmit={handleSubmit}></form>
+          </div>
+          <div className="row">
+            <div className="col-md-12">
+              <Button type="submit" primary>
+                Submit
+              </Button>
             </div>
-            <div className="row">
-              <div className="col-md-12">
-                <Button type="button" onClick={this.cancel}>
-                  Cancel
-                </Button>
-                <Button type="submit" primary>
-                  Submit
-                </Button>
-              </div>
-            </div>
-          </form>
-        </div>
+          </div>
+        </form>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 EditPage.defaultProps = {};
 EditPage.propTypes = {};
